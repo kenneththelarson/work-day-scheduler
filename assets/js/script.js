23a8be5f045c524d320222834560e611238a19cb
Kenny <kenneththelarson@gmail.com>
2020-12-06 14:24:57 -0700

    updated time checks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-var timeColor = function () {
-    var currentTime = moment().format("H");
-    currentTime = parseInt(currentTime);
+var tasks = {};
 
-    var hours = [$("#hour1"), $("#hour2"), $("#hour3"), $("#hour4"), $("#hour5"), $("#hour6"), $("#hour7"), $("#hour8"), $("#hour9")];
-    var times = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var currentTime = moment().format("H");
+currentTime = parseInt(currentTime);
+var hours = [$("#hour1"), $("#hour2"), $("#hour3"), $("#hour4"), $("#hour5"), $("#hour6"), $("#hour7"), $("#hour8"), $("#hour9")];
+var times = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+var timeColor = function () {
 
     for (i = 0; i < hours.length; i++) {
         if (times[i] < currentTime) {
